0c7b3de45ecbc032c05acd6ffbf839323ad710a1
JamieHa8794 <JamieHa8794@gmail.com>
2022-07-28 23:34:50 -0400

    createUser

--- server.js
+++ server.js
@@ -9,17 +9,28 @@ const getPlaces = async() =>{
     return (await client.query('SELECT * FROM "place";')).rows;
 };
 
+const createUser = async({ name }) =>{
+    return (await client.query('INSERT INTO "user"(name) VALUES($1) RETURNING *;', [name])).rows[0];
+}
+
 const syncAndSeed = async ()=>{
     const SQL = `
         DROP TABLE IF EXISTS "user";
+        DROP TABLE IF EXISTS "place";
         CREATE TABLE "user"(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(20) NOT NULL UNIQUE
+        );
+        CREATE TABLE "place"(
             id SERIAL PRIMARY KEY,
             name VARCHAR(20) NOT NULL UNIQUE
         );
         INSERT INTO "user"(name) VALUES('moe');
         INSERT INTO "user"(name) VALUES('ethyl');
         INSERT INTO "user"(name) VALUES('lucy');
-        INSERT INTO "user"(name) VALUES('curly');
+        INSERT INTO "place"(name) VALUES('NYC');
+        INSERT INTO "place"(name) VALUES('Chicago');
+        INSERT INTO "place"(name) VALUES('Houston');
     `
     await client.query(SQL);
 }
@@ -27,7 +38,10 @@ const init = async () =>{
     try{
         await client.connect();
         await syncAndSeed();
+        const curly = await createUser({name: 'curly'})
+        console.log(curly)
         console.log(await getUsers());
+        console.log(await getPlaces());
     }
     catch(err){
         console.log(err)
