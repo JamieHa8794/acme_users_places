b6e6a09b042ba44cffb0e2e8f8e78b86d096fac1
JamieHa8794 <JamieHa8794@gmail.com>
2022-07-28 23:17:36 -0400

    seed data

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const { Client }= require('pg');
 const client = new Client(process.env.DATABASE_URL || 'postgress://localhost/aceme_users_places');
 
+const getUsers = async() =>{
+    return (await client.query('SELECT * FROM "user";')).rows;
+};
+
+const getPlaces = async() =>{
+    return (await client.query('SELECT * FROM "place";')).rows;
+};
 
 const syncAndSeed = async ()=>{
     const SQL = `
@@ -10,13 +17,17 @@ const syncAndSeed = async ()=>{
             name VARCHAR(20) NOT NULL UNIQUE
         );
         INSERT INTO "user"(name) VALUES('moe');
+        INSERT INTO "user"(name) VALUES('ethyl');
+        INSERT INTO "user"(name) VALUES('lucy');
+        INSERT INTO "user"(name) VALUES('curly');
     `
     await client.query(SQL);
 }
 const init = async () =>{
     try{
         await client.connect();
-        await syncAndSeed()
+        await syncAndSeed();
+        console.log(await getUsers());
     }
     catch(err){
         console.log(err)
