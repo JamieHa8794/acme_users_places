659fb5d5d237afd912602e992a7f3e71f9122e25
JamieHa8794 <JamieHa8794@gmail.com>
2022-07-31 15:17:32 -0400

    refactor and set up user & places pages

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const {client, syncAndSeed, getUsers, getPlaces, createUser, deleteUser} = require('./db')
 const express = require('express')
 const app = express();
+const path = require('path')
+
+app.use('/public', express.static(path.join(__dirname,'public')))
+
+const nav = ({users, places}) =>{
+    return(`
+        <nav>
+            <a href='/'>Home</a>
+            <a href='/Users'>Users (${users.length})</a>
+            <a href='/Places'>Places (${places.length})</a>
+        </nav>
+    `)
+}
+
+const head = ({title})=>{
+    return(`
+        <head>
+            <link rel='stylesheet' href='/public/styles.css'/>
+            <title>${title}</title>
+        </head>
+    `)
+}
 
 app.get('/', async (req, res, next)=>{
     try{
@@ -11,20 +33,9 @@ app.get('/', async (req, res, next)=>{
         ])
         const HTML = `
             <html>
-            <head>
-            </head>
+            ${head({title: 'Acme Home'})}
             <body>
-                <ul>
-                    <li>
-                    <a href='/'>Home</a>
-                    </li>
-                    <li>
-                    <a href='/Users'>Users (${users.length})</a>
-                    </li>
-                    <li>
-                    <a href='/Places'>Places (${places.length})</a>
-                    </li>
-                </ul>
+                ${nav({users, places})}
                 <h1>
                 Welcome to Acme Users and Places
                 </h1>
@@ -38,8 +49,79 @@ app.get('/', async (req, res, next)=>{
     }
 })
 
+app.get('/users', async(req, res, next)=>{
+    try{
+        const [users, places] = await Promise.all([
+            getUsers(),
+            getPlaces(),
+        ])
+        const HTML = `
+            <html>
+            ${head({title: 'Users'})}
+            <body>
+                <nav>
+                    <a href='/'>Home</a>
+                    <a href='/Users'>Users (${users.length})</a>
+                    <a href='/Places'>Places (${places.length})</a>
+                </nav>
+                <h1>
+                Acme Users
+                </h1>
+                <ul>
+                ${users.map(user =>{
+                    return(`
+                        <li>
+                            ${user.name}
+                        </li>
+                    `)
+                }).join('')}
+                </ul>
+            </body>
+            </html>
+        `
+        res.send(HTML)
 
 
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+
+app.get('/places', async(req, res, next)=>{
+    try{
+        const [users, places] = await Promise.all([
+            getUsers(),
+            getPlaces(),
+        ])
+        const HTML = `
+            <html>
+            ${head({title: 'Places'})}
+            <body>
+                ${nav({users, places})}
+                <h1>
+                Acme Places
+                </h1>
+                <ul>
+                ${places.map(place =>{
+                    return(`
+                        <li>
+                            ${place.name}
+                        </li>
+                    `)
+                }).join('')}
+                </ul>
+            </body>
+            </html>
+        `
+        res.send(HTML)
+
+
+    }
+    catch(err){
+        console.log(err)
+    }
+})
 const init = async () =>{
     try{
         await client.connect();
