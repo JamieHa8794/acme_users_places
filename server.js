805cbc244b0c17e0bf41ffce8bf0452df968f2a7
JamieHa8794 <JamieHa8794@gmail.com>
2022-07-31 13:56:13 -0400

    refactor

--- server.js
+++ server.js
@@ -1,42 +1,6 @@
-const { Client }= require('pg');
-const client = new Client(process.env.DATABASE_URL || 'postgress://localhost/aceme_users_places');
+const {client, syncAndSeed, getUsers, getPlaces, createUser, deleteUser} = require('./db')
 
-const getUsers = async() =>{
-    return (await client.query('SELECT * FROM "user";')).rows;
-};
 
-const getPlaces = async() =>{
-    return (await client.query('SELECT * FROM "place";')).rows;
-};
-
-const createUser = async({ name }) =>{
-    return (await client.query('INSERT INTO "user"(name) VALUES($1) RETURNING *;', [name])).rows[0];
-}
-
-const deleteUser = async (id) =>{
-    await client.query('DELETE FROM "user" WHERE id=$1', [id])
-}
-const syncAndSeed = async ()=>{
-    const SQL = `
-        DROP TABLE IF EXISTS "user";
-        DROP TABLE IF EXISTS "place";
-        CREATE TABLE "user"(
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(20) NOT NULL UNIQUE
-        );
-        CREATE TABLE "place"(
-            id SERIAL PRIMARY KEY,
-            name VARCHAR(20) NOT NULL UNIQUE
-        );
-        INSERT INTO "user"(name) VALUES('moe');
-        INSERT INTO "user"(name) VALUES('ethyl');
-        INSERT INTO "user"(name) VALUES('lucy');
-        INSERT INTO "place"(name) VALUES('NYC');
-        INSERT INTO "place"(name) VALUES('Chicago');
-        INSERT INTO "place"(name) VALUES('Houston');
-    `
-    await client.query(SQL);
-}
 const init = async () =>{
     try{
         await client.connect();
